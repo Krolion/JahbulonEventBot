8450bdf8c7385aca9e76bac26dd1661ffc201a68
angelakazanskaia <a.a.nersisian@gmail.com>
2020-09-18 00:04:32 +0300

    test commit for db

---
+++ server_service/src/main/java/dbpackage/entity/Event.java
@@ -0,0 +1,33 @@
+package dbpackage.entity;
+
+import lombok.*;
+
+import java.util.Set;
+import javax.persistence.*;
+
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private int participationChatId;
+
+    private int orgChatId;
+
+  /*  @OneToMany(mappedBy = "answeredQuestion")
+    private Set<AnsweredQuestion> answeredQuestion;
+    @OneToMany(mappedBy = "unansweredQuestion")
+    private Set<UnansweredQuestion> unansweredQuestion;
+*/
+
+
+}
