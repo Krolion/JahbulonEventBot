1eef31626bedc4167bdc51d79b4f4cbc21721eec
Александр Плеханов <Plehalex09@gmail.com>
2020-09-17 22:10:46 +0300

    Pre final merge

---
+++ master_bot_service/src/test/java/com/example/master_bot_service/MasterBotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.master_bot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterBotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ organizers_bot_service/src/test/java/com/example/organizers_bot_service/OrganizersBotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.organizers_bot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizersBotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
