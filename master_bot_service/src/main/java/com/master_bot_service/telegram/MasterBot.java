2a881dd9e7d4adfb4bc1b3baf3a0f2289f13640c
Krolion <bendgamin7@gmail.com>
2020-09-19 21:16:47 +0300

    Добавлена поддержка сессий

--- master_bot_service/src/main/java/com/master_bot_service/telegram/MasterBot.java
+++ master_bot_service/src/main/java/com/master_bot_service/telegram/MasterBot.java
@@ -3,10 +3,9 @@ package com.master_bot_service.telegram;
 import com.master_bot_service.data.Chats;
 import com.master_bot_service.telegram.parser.UserMessageParser;
 import com.master_bot_service.data.Event;
+import com.master_bot_service.telegram.session.EventSession;
 import com.master_bot_service.utils.Poster;
 import lombok.SneakyThrows;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -14,6 +13,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import javax.inject.Singleton;
+import java.util.HashMap;
 
 @Component
 @Singleton
@@ -24,6 +24,7 @@ public class MasterBot extends TelegramLongPollingBot {
     public SendMessage lastMessage = null;
     public int a = 0;
     public Object lastUpdate;
+    public HashMap<String, EventSession> currentSessions = new HashMap<>();
     private final UserMessageParser createEventCommandParser = new UserMessageParser("create_event");
     private final UserMessageParser chatListCommandParser = new UserMessageParser("chat_list");
 
@@ -31,6 +32,11 @@ public class MasterBot extends TelegramLongPollingBot {
     @SneakyThrows
     public void onUpdateReceived(Update update) {
         lastUpdate = update;
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        if (currentSessions.containsKey(update.getMessage().getFrom().getUserName())) {
+            this.continueSession(update);
+            return;
+        }
         boolean flag = false;
         RestTemplate restTemplate = new RestTemplate();
         try {
@@ -40,7 +46,6 @@ public class MasterBot extends TelegramLongPollingBot {
             // Логика если есть команда /question в начале
             Chats pChats = restTemplate.getForObject(server + "ochats", Chats.class);
             Chats oChats = restTemplate.getForObject(server + "pchats", Chats.class);
-            SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
             assert oChats != null;
             assert pChats != null;
             sendMessage.setText("Participants chats \n" + pChats.chats.toString()
@@ -53,45 +58,48 @@ public class MasterBot extends TelegramLongPollingBot {
             flag = createEventCommandParser.parseMessage(update.getMessage().getText()).isCommand;
         } catch (Exception ignored) {}
         if (flag) {
-            long orgsChatId = -1;
-            long participantsChatId = -1;
-            SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
-            sendMessage.setText("Ошибочка вышла");
-            try {
-                StringBuilder orgsChatIdString = new StringBuilder();
-                StringBuilder participantsChatIdString = new StringBuilder();
-                int f = 0;
-                for (int i = 0; i < createEventCommandParser.text.length(); ++i) {
-                    if (' ' == createEventCommandParser.text.charAt(i)) {
-                        f++;
-                        i++;
-                    }
-                    if (f == 0) {
-                        orgsChatIdString.append(createEventCommandParser.text.charAt(i));
-                    }
-                    if (f == 1) {
-                        participantsChatIdString.append(createEventCommandParser.text.charAt(i));
-                    }
-                }
-                orgsChatId = Integer.parseInt(orgsChatIdString.toString());
-                participantsChatId = Integer.parseInt(participantsChatIdString.toString());
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (createEventCommandParser.text.equals("")){
+                EventSession eventSession = new EventSession(update.getMessage().getFrom().getUserName());
+                currentSessions.put(eventSession.username, eventSession);
+                sendMessage.setText("Введите id чата участников.");
             }
-            String s = (String) Poster.builder().aClassObject(Event.class)
-                    .aClassReturn(String.class)
-                    .object(Event.builder().orgsChatId(orgsChatId)
-                                           .participantsChatId(participantsChatId)
-                                           .eventId(-1).build())
-                    .url(server + "new_event")
-                    .build().post();
-            sendMessage.setText(s);
+            else {
+                sendMessage.setText("В данной версии /create_event не нуждается в параметрах. " +
+                        "Введите команду /create_event без других значений.");
+            }
             lastMessage = sendMessage;
             try {
                 execute(sendMessage);
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void continueSession(Update update) {
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        String s = currentSessions.get(update.getMessage().getFrom().getUserName()).continueSession(update);
+        if (currentSessions.get(update.getMessage().getFrom().getUserName()).stage ==
+                currentSessions.get(update.getMessage().getFrom().getUserName()).final_stage) {
+            currentSessions.remove(update.getMessage().getFrom().getUserName());
+        }
+        if (s.equals("")) {
+            sendMessage.setText("Сессия остановлена. Все изменения удалены.");
+            currentSessions.remove(update.getMessage().getFrom().getUserName());
+            lastMessage = sendMessage;
+            try {
+                execute(sendMessage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        sendMessage.setText(s);
+        lastMessage = sendMessage;
+        try {
+            execute(sendMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
