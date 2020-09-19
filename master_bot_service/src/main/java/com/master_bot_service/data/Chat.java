9af165c91c361f341fa8032eda66c1a5ecf321c6
Krolion <bendgamin7@gmail.com>
2020-09-19 14:37:29 +0300

    Added crucial dependency

--- master_bot_service/src/main/java/com/master_bot_service/data/Chat.java
+++ master_bot_service/src/main/java/com/master_bot_service/data/Chat.java
@@ -1,10 +1,14 @@
 package com.master_bot_service.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Chat {
     public long chat_id;
 }
